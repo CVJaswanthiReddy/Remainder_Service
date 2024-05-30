a83f7669fc0759725c72cfb2ea062805eaf33494
CVJaswanthiReddy <jaswanthireddy95@gmail.com>
2024-05-30 12:46:27 +0530

    code

--- src/index.js
+++ src/index.js
@@ -2,22 +2,37 @@ const express=require('express');
 
 const bodyParser=require('body-parser');
 const{PORT}=require('./config/server-config')
-const{sendBasicEmail}= require('./services/email-service');
-const setupAndStartServer=()=>{
+const{createChannel}= require('./config/server-config');
+const TicketController = require('./controllers/ticket-controller');
+
+const cron = require('node-cron');
+const jobs = require('./utilis/job');
+const m= require('./models/notificationticket')
+
+//const{sendBasicEmail}= require('./services/email-service');
+const setupAndStartServer=async()=>{
     const app=express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
+    //const channel= await createChannel();
+    app.post('/api/v1/tickets', TicketController.create);
     app.listen(PORT,()=>{
         console.log(`server started at PORT ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com',
-            'jaswanthireddy4@gmail.com',
-            'this is a testing email',
-            'hey hi how are you'
-        )
+
+       // jobs();
+
+      // console.log(m());
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'jaswanthireddy4@gmail.com',
+        //     'this is a testing email',
+        //     'hey hi how are you'
+        // )
+
+        
     })
 }
 
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,27 +1,79 @@
-const sender= require('../config/email-config');
+const sender = require('../config/email-config');
+const TicketRepository = require('../repository/ticket-repository');
 
-const sendBasicEmail=async (mailFrom , mailTo , mailSubject, mailBody) =>{
+const repo = new TicketRepository();
+
+const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
     try {
-        const response= await sender.sendMail({
-            from:mailFrom,
-            to:mailTo,
-            subject:mailSubject,
-            text:mailBody
-        })
-        console.log(response)
+        const response = await sender.sendMail({
+            from: mailFrom,
+            to: mailTo,
+            subject: mailSubject,
+            text: mailBody
+        });
+        console.log(response);
     } catch (error) {
         console.log(error);
     }
-   
 }
 
-module.exports={
-    sendBasicEmail
+const fetchPendingEmails = async (timestamp) => {
+    try {
+        const response = await repo.get({status: "PENDING"});
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+const updateTicket = async (ticketId, data) => {
+    try {
+        const response = await repo.update(ticketId, data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const createNotification = async (data) => {
+    try {
+        console.log(data);
+        const response = await repo.create(data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const subscribeEvents = async (payload) => {
+    let service = payload.service;
+    let data = payload.data;
+    switch(service) {
+        case 'CREATE_TICKET':
+            await createNotification(data);
+            break;
+        case 'SEND_BASIC_MAIL':
+            await sendBasicEmail(data);
+            break;
+        default: 
+            console.log('No valid event received');
+            break;
+    }
+}
+
+module.exports = {
+    sendBasicEmail,
+    fetchPendingEmails,
+    createNotification,
+    updateTicket,
+    subscribeEvents
+}
+
 
 /**
  * SMTP -> a@b.com
- * reciever -> d@e.com
+ * receiver-> d@e.com
  * 
- * from -> support@gmail.com
+ * from: support@noti.com
  */
